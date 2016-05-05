76998640663f8a001e8b9bf5fa68c1ae9027264e
bohdanos <bohdanos1993@gmail.com>
2016-05-05 19:15:24 +0300

    Create QuestionService, QuestionServiceImpl Change QuestionController By Bohdan , Nikita

---
+++ src/main/java/ua/netcracker/hr_system/model/service/serviceInterface/QuestionService.java
@@ -0,0 +1,26 @@
+package ua.netcracker.hr_system.model.service.serviceInterface;
+
+import org.springframework.stereotype.Service;
+import ua.netcracker.hr_system.model.entity.Question;
+
+import java.util.Collection;
+
+/**
+ * Created by bohda on 05.05.2016.
+ */
+@Service
+public interface QuestionService {
+    Collection<Question> getAllMandatory();
+
+    Collection<Question> getAll();
+
+    Question get(int id);
+
+    boolean save(Question question);
+
+    boolean update(Question question);
+
+    boolean remove(Question question);
+
+
+}
