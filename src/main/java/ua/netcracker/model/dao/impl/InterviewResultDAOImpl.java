e6f4e1dd35990dc91e3349aba72d2619cdacc6ec
AlyonaB <alyona.bilous@gmail.com>
2016-05-08 16:51:42 +0300

    Added methods to InterviewResultDAOImpl

--- src/main/java/ua/netcracker/model/dao/impl/InterviewResultDAOImpl.java
+++ src/main/java/ua/netcracker/model/dao/impl/InterviewResultDAOImpl.java
@@ -5,20 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ua.netcracker.model.dao.InterviewResultDAO;
+import ua.netcracker.model.entity.InterviewResult;
+import ua.netcracker.model.entity.Recommendation;
 
 import javax.sql.DataSource;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Alex on 07.05.2016.
+ * Changed by Alyona on 08.05.2016.
  */
 @Repository("InterviewResultDao")
 public class InterviewResultDAOImpl implements InterviewResultDAO {
     private static final Logger LOGGER = Logger.getLogger(InterviewResultDAOImpl.class);
-    private static final String FIND_MARK = "Select mark, id_interviewer from \"hr_system\".interview_result " +
+    private static final String FIND_MARK = "Select mark, interviewer_id from \"hr_system\".interview_result " +
             "where candidate_id = ";
     private static final String FIND_RECOMMENDATION =
             "Select r.value, i.interviewer_id from \"hr_system\".interview_result i " +
@@ -26,6 +26,39 @@ public class InterviewResultDAOImpl implements InterviewResultDAO {
                     "on i.recommendation_id = r.id where i.candidate_id = ";
     private static final String FIND_COMMENT =
             "Select response from \"hr_system\".interview_result where i.candidate_id = ";
+    private static final String FIND_ALL = "Select interviewer_id, mark, comment, recommendation_id" +
+            "from \"hr_system\".interview_result where candidate_id = ";
+    private static final String CREATE = "Insert into \"hr_system\".interview_result " +
+            "(interviewer_id, candidate_id, mark, comment, recommendation_id) values (?, ?, ?, ?, ?)";
+    private static final String UPDATE = "UPDATE \"hr_system\".interview_result SET " +
+            "(mark, comment, recommendation_id)=(?,?,?) " +
+            "WHERE interviewer_id = ? AND candidate_id = ?";
+
+    @Override
+    public Collection<InterviewResult> findResultsByCandidateId(Integer candidateId) {
+        List<InterviewResult> results = new ArrayList<>();
+        try{
+            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+            List<Map<String, Object>> rows = jdbcTemplate.queryForList(FIND_ALL + candidateId);
+            for (Map<String, Object> row : rows){
+                InterviewResult interviewResult = new InterviewResult();
+                interviewResult.setInterviewerId((int)row.get("interviewer_id"));
+                interviewResult.setMark((int)row.get("mark"));
+                interviewResult.setComment((String) row.get("comment"));
+                Recommendation[] recommendations = Recommendation.values();
+                for (Recommendation r : recommendations){
+                    if (r.getId() == row.get("recommendation_id")){
+                        interviewResult.setRecommendation(r.toString());
+                    }
+                }
+            }
+
+        } catch (Exception e){
+            LOGGER.error("Error: " + e);
+        }
+
+        return results;
+    }
 
 
     @Autowired
@@ -78,10 +111,37 @@ public class InterviewResultDAOImpl implements InterviewResultDAO {
         return comments;
     }
 
+    @Override
+    public boolean createInterviewResult(Integer candidate_id, InterviewResult interviewResult) {
+        try {
+            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+            jdbcTemplate.update(CREATE, interviewResult.getInterviewerId(), candidate_id,
+                    interviewResult.getMark(), interviewResult.getComment(),
+                    Recommendation.valueOf(interviewResult.getRecommendation()).getId());
+            return true;
+        } catch (Exception e) {
+            LOGGER.error("Error:" + e);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateInterviewResult(Integer candidate_id, InterviewResult interviewResult) {
+        try {
+            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+            jdbcTemplate.update(UPDATE, interviewResult.getMark(), interviewResult.getComment(),
+                    Recommendation.valueOf(interviewResult.getRecommendation()).getId(),
+                    interviewResult.getInterviewerId(), candidate_id);
+            return true;
+        } catch (Exception e) {
+            LOGGER.error("Error:" + e);
+        }
+        return false;
+    }
+
 
     @Override
     public Collection findAll() {
-        //////
 
         return null;
     }
