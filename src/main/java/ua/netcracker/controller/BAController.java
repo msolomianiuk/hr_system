a1b28cfff5df65bd08cc72f25d06cf69f6fd80fa
KingAlexJ8 <KingAlexJ8@ukr.net>
2016-05-09 18:07:01 +0300

    reload ba and dev Controller

--- src/main/java/ua/netcracker/controller/BAController.java
+++ src/main/java/ua/netcracker/controller/BAController.java
@@ -1,9 +1,14 @@
 package ua.netcracker.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import ua.netcracker.model.entity.Candidate;
+import ua.netcracker.model.service.CandidateService;
 
 /**
  * Class for processing requests related to ba
@@ -13,9 +18,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class BAController {
+    @Autowired
+    private CandidateService candidateService;
 
     @RequestMapping(value = "ba/interview_page", method = RequestMethod.GET)
     public String baMainPage() {
         return "interviewPage";
+    }
+
+    @RequestMapping(value = "ba/getStudent", method = RequestMethod.GET)
+    public ResponseEntity<Candidate> getCandidate(@RequestParam int id) {
+        Candidate candidate = candidateService.getCandidateById(id);
+        if (candidate == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(candidate, HttpStatus.OK);
     }
 }
