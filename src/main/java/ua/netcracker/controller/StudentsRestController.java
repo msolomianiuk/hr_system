82fb7a048b5c52c392599bc49e2cc05d21136860
michael <micsol95@gmail.com>
2016-05-10 12:31:38 +0300

    added filtering method to StudentsRestController

--- src/main/java/ua/netcracker/controller/StudentsRestController.java
+++ src/main/java/ua/netcracker/controller/StudentsRestController.java
@@ -9,8 +9,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ua.netcracker.model.entity.Answer;
 import ua.netcracker.model.entity.Candidate;
+import ua.netcracker.model.filtering.SimpleFilter;
 import ua.netcracker.model.service.CandidateService;
 
 import java.util.List;
@@ -34,6 +37,25 @@ public class StudentsRestController {
             return new ResponseEntity<List<Candidate>>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<List<Candidate>>(students, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/getStudents/filter")
+    public ResponseEntity<List<Candidate>> filterStudents(@RequestParam List<Answer> list) {
+
+        List<Candidate> students = (List<Candidate>) candidateService.getAllCandidates();
+        List<Candidate> filtered = students;
+
+        if (students.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        if (!list.isEmpty()) {
+            SimpleFilter filter = new SimpleFilter();
+            filter.setAnswerList(list);
+            filtered = filter.filter(students);
+        }
+
+        return new ResponseEntity<>(filtered, HttpStatus.OK);
     }
 
 }
