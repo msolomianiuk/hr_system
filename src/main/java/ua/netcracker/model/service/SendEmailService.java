42fada6a47e73e469a08c6e15f8a0575342d19df
Serhii <seriksol94@gmail.com>
2016-05-10 21:46:06 +0300

    refactor SendEmailService

--- src/main/java/ua/netcracker/model/service/SendEmailService.java
+++ src/main/java/ua/netcracker/model/service/SendEmailService.java
@@ -5,7 +5,7 @@ import ua.netcracker.model.entity.*;
 public interface SendEmailService {
     void sendLetterToEmails(String[] toEmails, String subject, String text);
     void sendEmailAboutSuccessfulRegistration(String[] toEmails);
-    void sendEmailAboutComingInterview(String[] toEmails, Role role, InterviewDaysDetails daysDetails);
-    void sendEmailToStudentsByStatus(String[] toEmails, Status status, InterviewDaysDetails daysDetails);
+    void sendEmailAboutComingInterview(Role role, InterviewDaysDetails daysDetails);
+    void sendEmailToStudentsByStatus(Status status, InterviewDaysDetails daysDetails);
     void sendEmailAboutCriticalError(String textError);
 }
