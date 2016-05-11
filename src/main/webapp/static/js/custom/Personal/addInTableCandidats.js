24468c4f1963893eadc5d01e4d92956c3e32b6f5
Ksenzod <Ksenzod@yandex.ru>
2016-05-11 15:58:59 +0300

    fix bag when 0 candidates

--- src/main/webapp/static/js/custom/Personal/addInTableCandidats.js
+++ src/main/webapp/static/js/custom/Personal/addInTableCandidats.js
@@ -1,39 +1,41 @@
-function addInTableCandidates(candidates){
-    candidates.forEach(
-        
-        function(candidate) {
-            if(candidate !== undefined ){
-                var answers;
-                var newTd = "<tr data-toggle='modal' data-target='#candidate-details' candidate-id=" + candidate.id + ">" +
-                    "<td>" + candidate.id + "</td>" +
-                    "<td>" + candidate.user.name + " " + candidate.user.surname + " " + candidate.user.patronymic + "</td>" +
-                    "<td>" + candidate.status + "</td>";
-                if (questionsIdArray == []) {
-                    newTd += "</tr>";
-                    $('table.candidate-list-table #students-table').append(newTd);
-                } else {
-                    for (i = 0; i < questionsIdArray.length; i++) {
-                        newTd += "<td class='question' question-id='" + questionsIdArray[i] + "'></td>";
-                    }
-                    newTd += "</tr>";
-                    $('table.candidate-list-table #students-table').append(newTd);
-
-                    answers = candidate.answers;
-
-                    answers.forEach(
-                        function (answer) {
-                            var td = $("table.candidate-list-table tr[candidate-id='" + candidate.id + "'] " +
-                                "td.question[question-id='" + answer.questionId + "']");
-                            if (td.html() !== "") {
-                                td.append(", ");
-                            }
-                            td.append(answer.value);
+function addInTableCandidates(candidates) {
+    if (candidates !== undefined) {
+        candidates.forEach(
+            function (candidate) {
+                if (candidate !== undefined) {
+                    var answers;
+                    var newTd = "<tr data-toggle='modal' data-target='#candidate-details' candidate-id=" + candidate.id + ">" +
+                        "<td>" + candidate.id + "</td>" +
+                        "<td>" + candidate.user.name + " " + candidate.user.surname + " " + candidate.user.patronymic + "</td>" +
+                        "<td>" + candidate.status + "</td>";
+                    if (questionsIdArray == []) {
+                        newTd += "</tr>";
+                        $('table.candidate-list-table #students-table').append(newTd);
+                    } else {
+                        for (i = 0; i < questionsIdArray.length; i++) {
+                            newTd += "<td class='question' question-id='" + questionsIdArray[i] + "'></td>";
                         }
-                    );
+                        newTd += "</tr>";
+                        $('table.candidate-list-table #students-table').append(newTd);
+
+                        answers = candidate.answers;
+
+                        answers.forEach(
+                            function (answer) {
+                                var td = $("table.candidate-list-table tr[candidate-id='" + candidate.id + "'] " +
+                                    "td.question[question-id='" + answer.questionId + "']");
+                                if (td.html() !== "") {
+                                    td.append(", ");
+                                }
+                                td.append(answer.value);
+                            }
+                        );
+                    }
+
                 }
             }
-        }
-    );
+        );
+    }
 
     $('#candidates-list').dataTable({
         "oLanguage": {
@@ -42,6 +44,4 @@ function addInTableCandidates(candidates){
     });
     // $('#candidates-list_filter label').html($('#candidates-list_filter label').html().replace('Search:',''));
     $('#candidates-list_filter label input').attr('placeholder', 'Search for...');
-    $('.loading').css('display', 'none');
-
 }
