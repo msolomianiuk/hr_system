23d41e84169619b4a460985dba809e6f5dedc701
michael <micsol95@gmail.com>
2016-05-17 00:47:17 +0300

    assure orderliness in Status.java

--- src/main/java/ua/netcracker/model/entity/Status.java
+++ src/main/java/ua/netcracker/model/entity/Status.java
@@ -4,7 +4,7 @@ package ua.netcracker.model.entity;
  * Created by Nikita on 28.04.2016.
  */
 public enum Status {
-    Rejected(0, "Rejected"), New(1, "New"), Ready(2, "Ready"), Interview(3, "Interview"), No_interview(4, "No_interview"), Interview_dated(5, "Interview_dated"), Interview_process(6, "Interview_process"), Interviews_passed(7, "Interviews_passed"), Trainee_accepted(8, "Trainee_accepted"), Job_accepted(9, "Job_accepted");
+    Rejected(0, "Rejected"), New(1, "New"), Ready(2, "Ready"), Interview(3, "Interview"), No_interview(4, "No_interview"), Interview_dated(5, "Interview_dated"), Interview_process(6, "Interview_process"), Interview_passed(7, "Interviews_passed"), Trainee_accepted(8, "Trainee_accepted"), Job_accepted(9, "Job_accepted");
 
     int id;
     String status;
