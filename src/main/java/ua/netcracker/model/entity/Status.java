b4a2edc6a03ed09f3337c37e025e8d5b4b2a8b6a
michael <micsol95@gmail.com>
2016-05-17 14:13:12 +0300

    corrected Status entity

--- src/main/java/ua/netcracker/model/entity/Status.java
+++ src/main/java/ua/netcracker/model/entity/Status.java
@@ -4,7 +4,7 @@ package ua.netcracker.model.entity;
  * Created by Nikita on 28.04.2016.
  */
 public enum Status {
-    Rejected(1, "Rejected"), New(2, "New"), Ready(3, "Ready"), Interview(4, "Interview"), No_interview(5, "No_interview"), Interview_dated(6, "Interview_dated"), Interview_process(7, "Interview_process"), Interview_passed(8, "Interviews_passed"), Trainee_accepted(9, "Trainee_accepted"), Job_accepted(10, "Job_accepted");
+    Rejected(1, "Rejected"), Ready(2, "Ready"), No_interview(3, "No_interview"), Interview(4, "Interview"), Interview_dated(5, "Interview_dated"), Interview_process(6, "Interview_process"), Interview_passed(7, "Interviews_passed"), Trainee_accepted(8, "Trainee_accepted"), Job_accepted(9, "Job_accepted");
 
     int id;
     String status;
